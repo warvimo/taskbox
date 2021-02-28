2c69afc45ff839e0a68c07e05f29a06ba5f6a589
Victor Moreira <warvimo@gmail.com>
2021-02-28 12:44:21 -0300

    task add

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -22,7 +22,7 @@ Default.args = {
     { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
     { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
     { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
-    { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
+    { ...TaskStories.Default.args.task, id: '6', title: 'Task 66' },
   ],
 };
 
