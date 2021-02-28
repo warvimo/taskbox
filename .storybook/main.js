13817d51ea9640b5a604a0e7be59165ed49c78a4
Victor Moreira <warvimo@gmail.com>
2021-02-28 12:28:21 -0300

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+// .storybook/main.js
+
+module.exports = {
+  //👇 Location of our stories
+  stories: ['../src/components/*.stories.js'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
+};
